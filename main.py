b0537fda7939e8029939c613ab1bda830d7f86d6
QiQiEva <qiqieva0@gmail.com>
2024-02-28 19:23:19 -0500

    add functions for expense and input data into db

--- main.py
+++ main.py
@@ -16,8 +16,57 @@ while True:
         description = input("Enter the description of expense: ")
         
         cur.execute("SELECT DISTINCT category FROM expenses")
+
+        categories = cur.fetchall()
+
+        print("Select a category by number: ")
+        for i, category in enumerate(categories):
+            print(f'{i + 1}.{category[0]}')
+        print(f'{len(categories) + 1}. Create a new category')
+
+        category_choice = int(input())
+        if category_choice == len(categories) + 1:
+            category = input("Enter the new category name: ")
+        else:
+            category = categories[category_choice - 1][0]
+
+        price = input("Enter the price of the expense: ")
+
+        cur.execute("INSERT INTO expenses (Date, description, category, price) VALUES (?, ?, ?, ?)", (date, description, category, price))
+        conn.commit()
+    
     elif choice == 2:
-        pass
+        print("Select an option:")
+        print("1. View all expenses")
+        print("2. View monthly expenses by category")
+
+        view_choice = int(input())
+        if view_choice == 1:
+            cur.execute("SELECT * FROM expenses")
+            expenses = cur.fetchall()
+            for expense in expenses:
+                print(expense)
+        elif view_choice == 2:
+            month = input("Enter the month (MM): ")
+            year = input("Enter the month (YYYY): ")
+            cur.execute("""
+                        SELECT category, SUM(price) FROM expenses 
+                        WHERE strftime('%m', Date) = ? AND strftime('%Y', Date) = ?
+                        GROUP BY category
+                        """, (month, year))
+            expenses = cur.fetchall()
+            for expense in expenses:
+                print(f'Category: {expense[0]}, Total: {expense[1]}')
+
+        else:
+            exit()
+
     else:
         exit()
         
+    repeat = input("Would you like to do something else (y/n)?\n")
+    if repeat.lower() != "y":
+        break
+
+conn.close() #close
+                   
